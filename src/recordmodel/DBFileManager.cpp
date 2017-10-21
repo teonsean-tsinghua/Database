e71d2c0dd04e86abc68f41741a7066d1ec5be242
Teon <fmj15@mails.tsinghua.edu.cn>
2017-10-21 13:40:51 +0800

    Added constructor and destructor for DBFileManager.

--- src/recordmodel/DBFileManager.cpp
+++ src/recordmodel/DBFileManager.cpp
@@ -1,1 +1,14 @@
+#include<DBFileManager.h>
 
+DBFileManager::DBFileManager(std::string root)
+{
+    this.root = root;
+    this.fileManager = new FileManager();
+    this.bufPageManager = new BufPageManager(this.fileManager);
+}
+
+DBFileManager::~DBFileManager()
+{
+    delete this.bufPageManager;
+    delete this.fileManager;
+}
--- src/recordmodel/DBFileManager.h
+++ src/recordmodel/DBFileManager.h
@@ -2,10 +2,21 @@
 #define FILEMANAGER_H_INCLUDED
 
 #include<../bufmanager/BufPageManager.h>
+#include<string>
 
 class DBFileManager
 {
+private:
+    FileManager* fileManager;
+    BufPageManager* bufPageManager;
+    std::string databaseName;
+    std::string tableName;
+    std::string root;
 
+public:
+    DBFileManager(std::string root);
+
+    ~DBFileManager();
 };
 
 #endif // FILEMANAGER_H_INCLUDED
