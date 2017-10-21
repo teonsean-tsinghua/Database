d6b4efbb810c6411c170c2617147c6e943bcef0d
Teon <fmj15@mails.tsinghua.edu.cn>
2017-10-22 02:06:53 +0800

    Provide terminal commands.

--- src/dbms.cpp
+++ src/dbms.cpp
@@ -5,23 +5,55 @@ using namespace std;
 
 int main()
 {
-
+    //DBFileManager fm("/home/teon/Document/Database");
     while(true)
     {
-        //char* command = new char[256];
         string command;
         cout << "\n>>";
-        cin >> command;
-        cout << command << endl;
-        if(command.compare("quit") == 0)
+        getline(cin, command);
+        if(command.compare("") == 1)
+        {
+            continue;
+        }
+        else if(command.compare("quit") == 0)
         {
             break;
         }
-        if(command.compare("createFile") == 0)
+        else if(command.compare("createFile") == 0)
         {
             cout << "\nPlease enter name of the file: ";
             string filename;
-            cin >> filename;
+            getline(cin, filename);
+        }
+        else if(command.compare("deleteFile") == 0)
+        {
+            cout << "\nPlease enter name of the file: ";
+            string filename;
+            getline(cin, filename);
+        }
+        else if(command.compare("closeFile") == 0)
+        {
+            cout << "\nPlease enter name of the file: ";
+            string filename;
+            getline(cin, filename);
+        }
+        else if(command.compare("updateFile") == 0)
+        {
+            cout << "\nPlease enter name of the file: ";
+            string filename;
+            getline(cin, filename);
+        }
+        else if(command.compare("insertRecord") == 0)
+        {
+        }
+        else if(command.compare("deleteRecord") == 0)
+        {
+        }
+        else if(command.compare("updateRecord") == 0)
+        {
+        }
+        else if(command.compare("findRecord") == 0)
+        {
         }
     }
     return 0;
