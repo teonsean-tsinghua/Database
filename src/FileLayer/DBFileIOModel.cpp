a1efcf72e152a8ecad782077a723ac3e1e66a522
Teon <fmj15@mails.tsinghua.edu.cn>
2017-11-06 21:38:43 +0800

    Finished DBFileIOModel. All the methods have been tested.

---
+++ src/FileLayer/DBFileIOModel.cpp
@@ -0,0 +1,93 @@
+#include"DBFileIOModel.h"
+#include<cstdio>
+
+DBFileIOModel* DBFileIOModel::instance = new DBFileIOModel();
+
+DBFileIOModel::DBFileIOModel()
+{
+    this->fm = new FileManager();
+    this->bpm = new BufPageManager(fm);
+}
+
+DBFileIOModel* DBFileIOModel::getInstance()
+{
+    if(instance == NULL)
+    {
+        instance = new DBFileIOModel();
+    }
+    return instance;
+}
+
+void DBFileIOModel::notifyPageUpdated(int index)
+{
+    bpm->access(index);
+    bpm->markDirty(index);
+}
+
+void DBFileIOModel::getKey(int index, int& fileID, int& pageID)
+{
+    bpm->getKey(index, fileID, pageID);
+}
+
+void DBFileIOModel::release(int index)
+{
+    bpm->release(index);
+}
+
+void DBFileIOModel::flush(int index)
+{
+    bpm->writeBack(index);
+}
+
+void DBFileIOModel::flushAll()
+{
+    bpm->close();
+}
+
+int DBFileIOModel::deleteFile(const char* name)
+{
+    if(remove(name) == 0)
+    {
+        return SUCCEED;
+    }
+    return ERROR;
+}
+
+int DBFileIOModel::createFile(const char* name)
+{
+    if(fm->createFile(name))
+    {
+        return SUCCEED;
+    }
+    return ERROR;
+}
+
+int DBFileIOModel::openFile(const char* name, int& fileID)
+{
+    if(fm->openFile(name, fileID))
+    {
+        return SUCCEED;
+    }
+    return ERROR;
+}
+
+int DBFileIOModel::closeFile(int fileID)
+{
+    if(fm->closeFile(fileID) == 0)
+    {
+        return SUCCEED;
+    }
+    return ERROR;
+}
+
+BufType DBFileIOModel::allocatePage(int fileID, int pageID, int& index)
+{
+    BufType ret = getPage(fileID, pageID, index);
+    memset(ret, 0, PAGE_SIZE);
+    return ret;
+}
+
+BufType DBFileIOModel::getPage(int fileID, int pageID, int& index)
+{
+    return bpm->getPage(fileID, pageID, index);
+}
---
+++ src/FileLayer/DBFileIOModel.h
@@ -0,0 +1,44 @@
+#ifndef DBFILEIOMODEL_H_INCLUDED
+#define DBFILEIOMODEL_H_INCLUDED
+
+#include"../bufmanager/BufPageManager.h"
+
+class DBFileIOModel
+{
+    static DBFileIOModel* instance;
+    BufPageManager* bpm;
+    FileManager* fm;
+
+    DBFileIOModel();
+
+public:
+    static DBFileIOModel* getInstance();
+
+    int createFile(const char* name);
+
+    int openFile(const char* name, int& fileID);
+
+    int closeFile(int fileID);
+
+    int deleteFile(const char* name);
+
+    BufType getPage(int fileID, int pageID, int& index);
+
+    BufType allocatePage(int fileID, int pageID, int& index);
+
+    void notifyPageUpdated(int index);
+
+    void flush(int index);
+
+    void getKey(int index, int& fileID, int& pageID);
+
+    void release(int index);
+
+    void flushAll();
+
+    const static int SUCCEED = 0;
+    const static int ERROR = 1;
+};
+
+#endif // DBFILEIOMODEL_H_INCLUDED
+
