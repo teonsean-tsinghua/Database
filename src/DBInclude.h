cc66c18a1cda067f4e943bd0c559f1b73e7f37fe
Teon <fmj15@mails.tsinghua.edu.cn>
2017-11-09 21:53:04 +0800

    Added DBInclude.h

---
+++ src/DBInclude.h
@@ -0,0 +1,18 @@
+#ifndef DBINCLUDE_H_INCLUDED
+#define DBINCLUDE_H_INCLUDED
+
+#include"utils/DBPrint.h"
+#include<string>
+#include<iostream>
+#include<cstring>
+#include<map>
+#include<vector>
+#include<cstdio>
+
+const static int SUCCEED = 0;
+
+const static int ERROR = SUCCEED - 1;
+
+const static int FIELD_ALREADY_EXIST = ERROR - 1;
+
+#endif // DBINCLUDE_H_INCLUDED
