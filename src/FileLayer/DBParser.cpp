e3d5d79fc6b71dd262fe5e5cd5f3724e61a0dafb
Teon <fmj15@mails.tsinghua.edu.cn>
2017-11-09 10:53:44 +0800

    Implemented writeRecordInfo()

--- src/FileLayer/DBParser.cpp
+++ src/FileLayer/DBParser.cpp
@@ -1,7 +1,7 @@
 #include"DBParser.h"
 #include<cstring>
 
-void DBParser::parseRecordInfo(std::map<std::string, int>& indexes,
+int DBParser::parseRecordInfo(std::map<std::string, int>& indexes,
                             std::vector<std::string>& names,
                             std::vector<int>& types,
                             std::vector<int>& offsets,
@@ -10,6 +10,13 @@ void DBParser::parseRecordInfo(std::map<std::string, int>& indexes,
 {
     char* end = cache + length;
     int index = 0, offset = 0, type = 0, name_length = 0;
+    if(!indexes.empty() ||
+       !names.empty() ||
+       !types.empty() ||
+       !offsets.empty())
+    {
+        return ERROR;
+    }
     while(cache < end)
     {
         sscanf(cache + RECORD_INFO_TYPE_OFFSET, "%d", &type);
@@ -25,14 +32,32 @@ void DBParser::parseRecordInfo(std::map<std::string, int>& indexes,
         cache += name_length;
         index++;
     }
+    return SUCCEED;
 }
 
-void DBParser::writeRecordInfo(std::map<std::string, int>& indexes,
-                            std::vector<std::string>& names,
-                            std::vector<int>& types,
-                            std::vector<int>& offsets,
-                            char* cache,
-                            int length)
+int DBParser::writeRecordInfo(std::vector<std::string>& names,
+                              std::vector<int>& types,
+                              char* cache,
+                              int& length)
 {
-
+    length = 0;
+    if(names.size() != types.size())
+    {
+        return ERROR;
+    }
+    for(int i = 0; i < names.size(); i++)
+    {
+        int name_length = names[i].size();
+        sprintf(cache + RECORD_INFO_TYPE_OFFSET, "%d", types[i]);
+        sprintf(cache + RECORD_INFO_NAME_LENGTH_OFFSET, "%d", name_length);
+        length += 2 * sizeof(int);
+        cache += RECORD_INFO_NAME_OFFSET;
+        if(names[i].copy(cache, name_length) != name_length)
+        {
+            return ERROR;
+        }
+        length += name_length;
+        cache += name_length;
+    }
+    return SUCCEED;
 }
--- src/FileLayer/DBParser.h
+++ src/FileLayer/DBParser.h
@@ -10,23 +10,24 @@
 class DBParser
 {
 public:
-    static void parseRecordInfo(std::map<std::string, int>& indexes,
+    static int parseRecordInfo(std::map<std::string, int>& indexes,
                                 std::vector<std::string>& names,
                                 std::vector<int>& types,
                                 std::vector<int>& offsets,
                                 char* cache,
                                 int length);
 
-    static void writeRecordInfo(std::map<std::string, int>& indexes,
-                                std::vector<std::string>& names,
-                                std::vector<int>& types,
-                                std::vector<int>& offsets,
-                                char* cache,
-                                int length);
+    static int writeRecordInfo(std::vector<std::string>& names,
+                               std::vector<int>& types,
+                               char* cache,
+                               int& length);
 
     const static int RECORD_INFO_TYPE_OFFSET = 0;
     const static int RECORD_INFO_NAME_LENGTH_OFFSET = RECORD_INFO_TYPE_OFFSET + sizeof(int);
     const static int RECORD_INFO_NAME_OFFSET = RECORD_INFO_NAME_LENGTH_OFFSET + sizeof(int);
+
+    const static int SUCCEED = 0;
+    const static int ERROR = SUCCEED + 1;
 };
 
 #endif // DBPARSER_H_INCLUDED
