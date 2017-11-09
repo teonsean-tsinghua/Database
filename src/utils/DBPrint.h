f2c14421c95bc2374dcd3a152c9146e02d89cac1
Teon <fmj15@mails.tsinghua.edu.cn>
2017-11-09 16:30:34 +0800

    Added utils to output message to console.

---
+++ src/utils/DBPrint.h
@@ -0,0 +1,23 @@
+#ifndef DBPRINT_H_INCLUDED
+#define DBPRINT_H_INCLUDED
+
+#include<string>
+#include<iostream>
+
+void DBLog(std::string msg);
+
+void DBLogLine(std::string msg = "");
+
+void DBPrint(std::string msg);
+
+void DBPrintLine(std::string msg = "");
+
+void DBLog(int msg);
+
+void DBLogLine(int msg = "");
+
+void DBPrint(int msg);
+
+void DBPrintLine(int msg = "");
+
+#endif // DBPRINT_H_INCLUDED
