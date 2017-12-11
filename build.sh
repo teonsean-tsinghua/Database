785a9509f6accfcebe74eee371f8c4a4808fab36
Teon <fmj15@mails.tsinghua.edu.cn>
2017-12-12 00:37:36 +0800

    Updated build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,33 @@
 #! /bin/bash
 cd src
 cd DatabaseLayer
+echo "============================================"
 echo "Generating lexer and parser files..."
 yacc -d parser.y
+if [ $? != 0 ] 
+then
+	echo "============================================"
+	echo "Failed."
+	exit
+fi
 lex lexer.l
+if [ $? != 0 ] 
+then
+	echo "============================================"
+	echo "Failed."
+	exit
+fi
 cd ..
+echo "Succeeded."
+echo "============================================"
 echo "Compiling source files..."
 make
+if [ $? != 0 ] 
+then
+	echo "============================================"
+	echo "Failed."
+	exit
+fi
 mv ./application ../dbms
+echo "============================================"
 echo "Succeed."
